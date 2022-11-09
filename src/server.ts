d0e54d4f0c2fb5e638b7d2b7b9827ae4cb18c9fc
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-11-09 01:08:39 -0300

    feat(server): add application instance

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 import deployConfig from './configs/deploy';
 
+import app from './app';
+
 const server = express();
+
+server.use(app);
 
 server.listen(deployConfig.port, () => console.log(`Server running on port ${deployConfig.port}`));
