b2beb80420c9bc6396f2fc68d108ef9fd4fcbaa7
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-11-09 01:01:53 -0300

    feat(app): create app instance. set general middlewares

---
+++ src/app/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+app.use(cors())
+app.use(helmet());
+
+export default app;
