00c9ba3ce65cdd82c0d41ad0193c22ac94e0114e
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-11-09 01:25:41 -0300

    feat(app): add auth router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import testRoutes from './tests/test.routes';
+import authRoutes from './auth/auth.routes';
 
 const router = Router();
 
 router.use('/tests', testRoutes);
+router.use('/auth', authRoutes);
 
 export default router;
