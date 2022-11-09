405f7f98a22cb75cf237602fb87d3aed760bf1ec
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-11-09 17:45:58 -0300

    feat(errors): add errors dictionary

---
+++ src/common/errors/errors.ts
@@ -0,0 +1,20 @@
+import userErrors from './user/user.error';
+
+const errors = {
+	auth: authErrors,
+	user: userErrors,
+}
+
+function flattenObject(object: Object, key: string = ''): any[] {
+	return Object.entries(object).reduce((acc, [newKey, value]) => {
+		if (typeof value === 'object') {
+			acc.push(...flattenObject(value, key + newKey));
+		} else {
+			acc.push([key + newKey, value]);
+		}
+
+		return acc;
+	}, <any>[]);
+}
+
+export default Object.fromEntries(flattenObject(errors));
