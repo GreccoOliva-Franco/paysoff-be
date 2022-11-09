0f85d71983dd9950190f74eeef67d6b669068522
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-11-09 01:04:32 -0300

    feat(app): add api router

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 
+import apiRoutes from './app.routes';
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(helmet());
+
+app.use('/api', apiRoutes);
 
 export default app;
