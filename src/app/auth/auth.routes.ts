7dfc05654e717f5368d58c189ca70714beb5e7be
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-11-10 00:51:21 -0300

    fix(auth): comment not implemented routes

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -5,6 +5,6 @@ import authController from './auth.controller';
 const router = Router();
 
 router.post('/register', authController.register);
-router.post('/login', authController.login);
+// router.post('/login', authController.login);
 
 export default router;
