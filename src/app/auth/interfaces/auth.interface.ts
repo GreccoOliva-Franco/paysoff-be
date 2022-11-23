fdbf0c748b10ce642a9ac713044b7ac42b28bb25
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-11-22 22:47:31 -0300

    feat(auth): add auth controller implementation interface

--- src/app/auth/interfaces/auth.interface.ts
+++ src/app/auth/interfaces/auth.interface.ts
@@ -1,3 +1,9 @@
+import { Request, Response } from "express";
+
+export interface IAuthController {
+	register: (req: Request, res: Response) => Promise<Response>;
+}
+
 export interface IAuthCredentials {
 	email: string;
 	password: string;
