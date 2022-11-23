da870a2f5a0091dc9ec8a93310e5f7f98b03afac
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-11-22 23:35:05 -0300

    feat(errors): add user already exists error

--- src/common/errors/user/user.error.ts
+++ src/common/errors/user/user.error.ts
@@ -1,1 +1,10 @@
-export default {}
+import { CustomError } from '../custom.error';
+
+const prefix = 'users-E00-';
+
+export class UserAlreadyExistsError extends CustomError {
+	constructor() {
+		super();
+		this.internalCode = prefix + '000';
+	}
+}
