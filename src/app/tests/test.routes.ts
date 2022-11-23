653e6fd3bf1b1f4de80118095e023cd61dd70ca7
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-11-22 23:37:36 -0300

    feat(tests): add endpoint to test database connection

--- src/app/tests/test.routes.ts
+++ src/app/tests/test.routes.ts
@@ -1,8 +1,16 @@
+// External modules
 import { Router, Request, Response } from 'express';
 import HttpCodes from 'http-status-codes';
+
+// Infrastructure
+import database from '../../infrastractures/database';
 
 const router = Router();
 
 router.get('/test', (_: Request, res: Response) => res.status(HttpCodes.OK).json({ success: true }))
+router.get('/database', (_: Request, res: Response) => {
+	const status = database.isInitialized;
 
+	return res.status(HttpCodes.OK).json({ database: { status } });
+})
 export default router;
