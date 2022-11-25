a9f47e36ad5f5bbc41d3cf71ac68dc29ccff3687
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-11-25 20:38:18 -0300

    feat(auth): add refresh controller logic

--- src/app/auth/controllers/auth.controller.ts
+++ src/app/auth/controllers/auth.controller.ts
@@ -6,7 +6,8 @@ import { IAuthController, IAuthCredentials } from '../interfaces/auth.interface'
 import authService from '../services/auth.service';
 
 import { UserAlreadyExistsError, UserInvalidCredentialsError } from '../../../common/errors/users/user.error';
-
+import { AuthTokenExpiredError, AuthTokenInvalidError } from '../../../common/errors/auth/auth.error';
+import { ErrorLogger } from '../../../common/loggers/error.logger';
 export class AuthController implements IAuthController {
 	constructor() { };
 
@@ -20,6 +21,7 @@ export class AuthController implements IAuthController {
 		} catch (error) {
 			if (error instanceof UserAlreadyExistsError) return res.status(httpCodes.BAD_REQUEST).json({ error: error.message });
 
+			ErrorLogger.logUncaughtError(error)
 			return res.status(httpCodes.INTERNAL_SERVER_ERROR).json();
 		}
 	}
@@ -34,9 +36,26 @@ export class AuthController implements IAuthController {
 		} catch (error) {
 			if (error instanceof UserInvalidCredentialsError) return res.status(httpCodes.UNAUTHORIZED).json({ error: error.errors });
 
+			ErrorLogger.logUncaughtError(error)
 			return res.status(httpCodes.INTERNAL_SERVER_ERROR).json();
 		}
 	};
+
+	async refresh(req: Request, res: Response): Promise<Response> {
+		try {
+			const { refreshToken } = req.body;
+
+			const tokens = await authService.refresh(refreshToken);
+
+			return res.status(httpCodes.OK).json(tokens);
+		} catch (error) {
+			if (error instanceof AuthTokenExpiredError) return res.status(httpCodes.UNAUTHORIZED).json({ error: error.message });
+			if (error instanceof AuthTokenInvalidError) return res.status(httpCodes.UNAUTHORIZED).json({ error: error.message });
+
+			ErrorLogger.logUncaughtError(error)
+			return res.status(httpCodes.INTERNAL_SERVER_ERROR).json();
+		}
+	}
 }
 
 export default new AuthController();
