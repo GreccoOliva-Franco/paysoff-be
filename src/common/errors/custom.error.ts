1c78640a9e5887a3b3bd707edd842e1b4f1fae99
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-11-25 18:58:32 -0300

    feat(errors): add custom error definition

--- src/common/errors/custom.error.ts
+++ src/common/errors/custom.error.ts
@@ -1,6 +1,18 @@
+import { ICustomError } from "./interaces/error.interface";
+
 export abstract class CustomError extends Error {
 	public name: string;
 	public message: string;
-	public stack?: string | undefined;
+	public stack?: string;
+	public errors: any[];
 	public internalCode: string;
+
+	constructor({ name, message, errors, stack, internalCode }: ICustomError) {
+		super(message);
+		this.name = name;
+		this.message = message;
+		if (errors) this.errors = errors;
+		this.stack = stack;
+		this.internalCode = internalCode;
+	}
 }
