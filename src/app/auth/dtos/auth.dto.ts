89b9e5275b745b85fa565482b2cc65ca48b668a2
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-11-25 19:24:33 -0300

    feat(auth): add register dto

--- src/app/auth/dtos/auth.dto.ts
+++ src/app/auth/dtos/auth.dto.ts
@@ -1,6 +1,22 @@
 import { IsString, IsNotEmpty, IsOptional, IsEmail, Length } from 'class-validator';
 import { MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH } from '../../../configs/business/users/user.config';
 
+export class AuthRegisterDto {
+	@IsString()
+	@IsNotEmpty()
+	username: string;
+
+	@IsString()
+	@IsEmail()
+	@IsNotEmpty()
+	email: string;
+
+	@IsString()
+	@IsNotEmpty()
+	@Length(MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH)
+	password: string;
+}
+
 export class AuthLoginDto {
 	@IsString()
 	@IsNotEmpty()
