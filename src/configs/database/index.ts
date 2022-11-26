36951d78d949040f9732e559208e7d95c966127c
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-11-26 02:46:43 -0300

    fix(database): trigger database connection depending on docker or local deploy

--- src/configs/database/index.ts
+++ src/configs/database/index.ts
@@ -5,15 +5,19 @@ import enviromentConfig from "../enviroment"
 
 dotenv.config({ path: enviromentConfig.path })
 
+let host: string;
+if (process.env.DEPLOY_CONFIG === 'docker') host = process.env.DATABASE_HOST_DOCKER!;
+if (process.env.DEPLOY_CONFIG === 'local') host = process.env.DATABASE_HOST_LOCAL!;
+
 const databaseConfig: PostgresConnectionOptions = {
 	type: 'postgres',
-	host: process.env.DATABASE_HOST!,
+	host: host!,
 	port: parseInt(process.env.DATABASE_PORT!),
 	username: process.env.DATABASE_USERNAME!,
 	password: process.env.DATABASE_PASSWORD!,
 	database: process.env.DATABASE_NAME!,
-	entities: ['src/app/**/entities/*.entity.ts'],
-	subscribers: ['src/app/**/entities/*.subscriber.ts'],
+	entities: ['dist/app/**/entities/*.entity.js'],
+	subscribers: ['dist/app/**/entities/*.subscriber.js'],
 	synchronize: true,
 	logging: false,
 }
