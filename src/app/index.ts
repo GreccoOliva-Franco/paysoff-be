f7be3260b20e01612af653a45454ac899fd36259
Grecco Oliva, Franco <srgrecco93@gmail.com>
2023-01-04 05:45:27 -0300

    feat(app): add morgan logger

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import helmet from 'helmet';
 import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 import apiRoutes from './app.routes';
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(helmet());
+app.use(morgan('dev'))
 
 app.use('/api', apiRoutes);
 
