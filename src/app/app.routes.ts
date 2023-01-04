8f2293e4435712eee5b3c785627a980b69fd58cf
Grecco Oliva, Franco <srgrecco93@gmail.com>
2023-01-04 09:29:33 -0300

    feat(app): add users endpoints

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
-import testRoutes from './tests/test.routes';
 import authRoutes from './auth/auth.routes';
+import testRoutes from './tests/test.routes';
+import userRoutes from './users/user.routes';
 
 const router = Router();
 
-router.use('/tests', testRoutes);
 router.use('/auth', authRoutes);
+router.use('/tests', testRoutes);
+router.use('/users', userRoutes);
 
 export default router;
