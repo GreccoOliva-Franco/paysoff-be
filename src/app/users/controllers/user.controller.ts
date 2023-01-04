7569ba6cd3785c621412870573abdea1823f98c8
Grecco Oliva, Franco <srgrecco93@gmail.com>
2023-01-04 09:29:13 -0300

    feat(users): add find user profile functionality

--- src/app/users/controllers/user.controller.ts
+++ src/app/users/controllers/user.controller.ts
@@ -1,23 +1,25 @@
 import { Request, Response } from "express";
 import httpCode from "http-status-codes";
 
-import userService, { UserService } from "../services/user.service";
+import { ErrorLogger } from "../../../common/loggers/error.logger";
+
+import userService from "../services/user.service";
 
 export class UserController {
-	private readonly service: UserService;
-
-	constructor() {
-		this.service = userService
-	}
+	constructor() { }
 
 	async findProfileUserById(req: Request, res: Response): Promise<Response> {
 		try {
-			const { userId } = req.params;
 
-			const user = await this.service.findProfileById(userId!);
+			const { userId } = req.params;
+			if (!userId) return res.status(httpCode.BAD_REQUEST).json({ error: '\"userId\" must be specified' })
+
+			const user = await userService.findProfileById(userId!);
 
 			return res.status(httpCode.OK).json(user);
 		} catch (error) {
+			ErrorLogger.logUncaughtError(error);
+
 			return res.status(httpCode.INTERNAL_SERVER_ERROR).json();
 		}
 	}
